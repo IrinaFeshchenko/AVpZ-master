841cf1c4eeb158cbc9a834a17bb7e1b7558f69e1
Лева Штанько <levalova2@gmail.com>
2020-11-16 14:37:36 +0300

    push_new

--- ShareMeet/Controllers/HomeController.cs
+++ ShareMeet/Controllers/HomeController.cs
@@ -31,7 +31,10 @@ namespace ShareMeet.Controllers
         {
             return View();
         }
-
+        public IActionResult vert()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
