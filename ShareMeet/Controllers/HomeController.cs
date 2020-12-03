72c6400e60420fae2651aebeec4c6fe862440a3e
IrinaFeshchenko <58722023+IrinaFeshchenko@users.noreply.github.com>
2020-12-03 02:33:16 +0200

    index

--- ShareMeet/Controllers/HomeController.cs
+++ ShareMeet/Controllers/HomeController.cs
@@ -12,14 +12,35 @@ namespace ShareMeet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private UsersContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,UsersContext context)
         {
             _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
+            var meetings = from meet in db.MeetUps
+                           select new MeetUp
+                           {
+                               Id_meetup = meet.Id_meetup,
+                               Name = meet.Name,
+                               Type = meet.Type,
+                               Description = meet.Description,
+                               companyId_company = meet.companyId_company,
+                               StartofSelection = meet.StartofSelection,
+                               FinishofSelection = meet.FinishofSelection,
+                               lng = meet.lng,
+                               lat = meet.lat,
+                               Adress = meet.Adress,
+                               Cost = meet.Cost
+
+                           };
+            var meets = db.MeetUps.Count();
+            ViewBag.count = meets;
+            ViewBag.data = meetings;
             return View();
         }
 
